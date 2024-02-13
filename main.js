34ab94edafeecf841c6f952325cd16a4a546311f
Savelii Kosevich <savva0512@gmail.com>
2024-02-13 23:24:08 +0300

    finish troll version

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // const base = document.querySelector("#base");
 
 const inputs = document.querySelectorAll("input");
-
+const au = document.querySelector("#sound");
 function handleUpdate() {
   const suffix = this.dataset.sizing || "";
 
@@ -11,3 +11,17 @@ function handleUpdate() {
 }
 inputs.forEach((input) => input.addEventListener("change", handleUpdate));
 inputs.forEach((input) => input.addEventListener("input", handleUpdate));
+
+inputs.forEach((input) => {
+  const handleChangeOnce = () => {
+    if (input.name === "blur") {
+      au.currentTime = 0;
+      au.play();
+    }
+
+    // Удаляем обработчик события после первого срабатывания
+    input.removeEventListener("change", handleChangeOnce);
+  };
+
+  input.addEventListener("change", handleChangeOnce);
+});
